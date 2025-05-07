70bd78b840a9f984dd4bda598195344ee6a31f5a
Jasper Mao <yabiye@outlook.com>
2025-05-07 15:46:51 +0800

    test

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir -p log
-nohup ./build/bin/MulticastReceive config/Channel_1.json > log/InstrumentDefinition.log 2>&1 &
-nohup ./build/bin/MulticastReceive config/Channel_2.json > log/SnapshotsRecovery.log 2>&1 &
-nohup ./build/bin/MulticastReceive config/Channel_3.json > log/Incremental.log 2>&1 &
-nohup ./build/bin/MulticastReceive config/Channel_4.json > log/IncrementalBak.log 2>&1 &
-nohup ./build/bin/MulticastReceive config/Channel_80.json > log/all.log 2>&1 &
+nohup ./build/bin/MulticastReceive config/1_InstrumentDefinition.json > log/1_InstrumentDefinition.log 2>&1 &
+nohup ./build/bin/MulticastReceive config/2_SnapshotsRecovery.json > log/2_SnapshotsRecovery.log 2>&1 &
+nohup ./build/bin/MulticastReceive config/3_Incremental.json > log/3_Incremental.log 2>&1 &
+nohup ./build/bin/MulticastReceive config/4_IncrementalBak.json > log/4_IncrementalBak.log 2>&1 &
+nohup ./build/bin/MulticastReceive config/5_Channel_80.json > log/5_all.log 2>&1 &
 echo "Started MulticastReceive. Log: log/multicast_receive.log"
